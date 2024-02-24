510ef9f84ebb5e841cd25ef3882d6b332049bb00
xM4ddy <59680197+xM4ddy@users.noreply.github.com>
2024-02-23 21:13:44 -0500

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,137 @@
 extern crate systemstat;
 
-use serde::Serialize;
-//use serde_json::Result;
-use serde_json;
 //use std::thread;
 //use std::time::Duration;
-use std::time::{Duration, SystemTime};
+use serde::Serialize;
+use serde_json;
+use std::time::Duration;
 use systemstat::{saturating_sub_bytes, Platform, System};
+
+struct StatServer {
+    sys: System,
+}
 
 #[derive(Serialize)]
 struct Stats {
-    battery: String,
-    ac: String,
-    memory: String,
+    power: Power,
+    memory: Memory,
     uptime: String,
     cpu_temp: String,
 }
 
-fn main() {
-    let seconds = Duration::from_secs(999999999999);
-    let start = SystemTime::now();
-    let sys = System::new();
+#[derive(Serialize)]
+struct Power {
+    percent: String,
+    ac_power: String,
+}
 
-    let mut json;
+#[derive(Serialize)]
+struct Memory {
+    mem_used: String,
+    mem_total: String,
+}
 
-    loop {
-        std::thread::sleep(Duration::new(1, 0));
+impl StatServer {
+    fn new() -> Self {
+        let sys = System::new();
 
-        let battery = match sys.battery_life() {
+        StatServer { sys }
+    }
+
+    fn stats(self: &Self) -> Stats {
+        let percent = match self.sys.battery_life() {
             Ok(battery) => (battery.remaining_capacity * 100.0).to_string(),
             Err(x) => x.to_string(),
         };
 
-        let ac = match sys.on_ac_power() {
+        let ac_power = match self.sys.on_ac_power() {
             Ok(power) => power.to_string(),
             Err(x) => x.to_string(),
         };
 
-        let memory = match sys.memory() {
-            Ok(mem) => format!(
-                "{}/{}",
-                saturating_sub_bytes(mem.total, mem.free),
-                mem.total
-            ),
+        let mem_used = match self.sys.memory() {
+            Ok(mem) => saturating_sub_bytes(mem.total, mem.free).to_string(),
             Err(x) => x.to_string(),
         };
 
-        let uptime = match sys.uptime() {
+        let mem_total = match self.sys.memory() {
+            Ok(mem) => mem.total.to_string(),
+            Err(x) => x.to_string(),
+        };
+
+        let uptime = match self.sys.uptime() {
             Ok(uptime) => format!("{:?}", uptime),
             Err(x) => x.to_string(),
         };
 
-        let cpu_temp = match sys.cpu_temp() {
+        let cpu_temp = match self.sys.cpu_temp() {
             Ok(cpu_temp) => cpu_temp.to_string(),
             Err(x) => x.to_string(),
         };
 
-        // match sys.cpu_load_aggregate() {
-        //     Ok(cpu) => {
-        //         println!("\nMeasuring CPU load...");
-        //         thread::sleep(Duration::from_secs(1));
-        //         let cpu = cpu.done().unwrap();
-        //         println!(
-        //             "CPU load: {}% user, {}% nice, {}% system, {}% intr, {}% idle ",
-        //             cpu.user * 100.0,
-        //             cpu.nice * 100.0,
-        //             cpu.system * 100.0,
-        //             cpu.interrupt * 100.0,
-        //             cpu.idle * 100.0
-        //         );
-        //     }
-        //     Err(x) => println!("\nCPU load: error: {}", x),
-        // }
-
-        // match sys.load_average() {
-        //     Ok(loadavg) => println!(
-        //         "\nLoad average: {} {} {}",
-        //         loadavg.one, loadavg.five, loadavg.fifteen
-        //     ),
-        //     Err(x) => println!("\nLoad average: error: {}", x),
-        // }
-
-        // let string = match sys.networks() {
-        //     Ok(netifs) => {
-        //         format!("\nNetwork interface statistics:");
-        //         for netif in netifs.values() {
-        //             format!(
-        //                 "{} statistics: ({:?})",
-        //                 netif.name,
-        //                 sys.network_stats(&netif.name)
-        //             );
-        //         }
-        //     }
-        //     Err(x) => println!("\nNetworks: error: {}", x),
-        // }
-
-        json = serde_json::to_string(&Stats {
-            battery,
-            ac,
-            memory,
+        Stats {
+            power: Power { percent, ac_power },
+            memory: Memory {
+                mem_used,
+                mem_total,
+            },
             uptime,
             cpu_temp,
-        })
-        .expect("Serialization failed");
-
-        println!("{}", json);
-
-        match start.elapsed() {
-            Ok(elapsed) if elapsed > seconds => {
-                return;
-            }
-            _ => (),
         }
     }
 }
+
+fn main() {
+    let stat = StatServer::new();
+
+    loop {
+        let mut s = stat.stats();
+        
+        println!("{}", to_json(&mut s));
+        
+        std::thread::sleep(Duration::new(1, 0));
+    }
+}
+
+fn to_json(stats: &mut Stats) -> String {
+    serde_json::to_string(stats).expect("Serialization failed")
+}
+
+// match sys.cpu_load_aggregate() {
+//     Ok(cpu) => {
+//         println!("\nMeasuring CPU load...");
+//         thread::sleep(Duration::from_secs(1));
+//         let cpu = cpu.done().unwrap();
+//         println!(
+//             "CPU load: {}% user, {}% nice, {}% system, {}% intr, {}% idle ",
+//             cpu.user * 100.0,
+//             cpu.nice * 100.0,
+//             cpu.system * 100.0,
+//             cpu.interrupt * 100.0,
+//             cpu.idle * 100.0
+//         );
+//     }
+//     Err(x) => println!("\nCPU load: error: {}", x),
+// }
+
+// match sys.load_average() {
+//     Ok(loadavg) => println!(
+//         "\nLoad average: {} {} {}",
+//         loadavg.one, loadavg.five, loadavg.fifteen
+//     ),
+//     Err(x) => println!("\nLoad average: error: {}", x),
+// }
+
+// let string = match sys.networks() {
+//     Ok(netifs) => {
+//         format!("\nNetwork interface statistics:");
+//         for netif in netifs.values() {
+//             format!(
+//                 "{} statistics: ({:?})",
+//                 netif.name,
+//                 sys.network_stats(&netif.name)
+//             );
+//         }
+//     }
+//     Err(x) => println!("\nNetworks: error: {}", x),
+// }
